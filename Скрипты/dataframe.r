67e39758ea44229f06204f4d0fa70a5754c3e02a
IlyaChakov <carpel-orders0c@icloud.com>
2024-11-23 00:14:48 +0300

    Update

--- Скрипты/dataframe.r
+++ Скрипты/dataframe.r
@@ -9,9 +9,11 @@ df <- read_csv("C:\\Users\\User\\Documents\\GitHub\\Ilya Chakov\\Aluminium-Marke
 
 df_summarized <- df %>%
   filter(Date >= 2010) %>%
-  group_by(country) %>%
+  group_by(Date) %>%
   summarise(total_value = sum(Value, na.rm = FALSE)) %>%
   arrange(desc(total_value))
+
+print(df_summarized)
 
 top_10 <- df_summarized %>%
   slice_max(order_by = total_value, n = 10)
@@ -25,6 +27,7 @@ fwrite(df_top_producers, file = "production.csv", sep = "\t", quote = FALSE)
 print(top_10)
 print(top_10$country)
 print(df_top_producers)
+print(df)
 
 print(countries)
 
@@ -57,15 +60,14 @@ countries <- c("China", "Russia", "Canada", "India", "United Arab Emirates", "Au
 
 print(countries)
 
+df_rest
+
 df_prod <- df %>%
   filter(Date >= 2010, country %in% countries)
 
 print(df_prod)
 
 fwrite(df_prod, file = "production.csv", sep = "\t", quote = FALSE)
-
-df_prod_mln <- df_prod
-
 print(df_prod_mln)
 
 df_prod_mln$Unit <- "mln tonnes"
@@ -82,4 +84,39 @@ fwrite(production_mln, file = "production_mln.csv", sep = "\t", quote = FALSE)
 
 production_mln <- production %>%
   production_mln$Unit <- "mln tonnes" %>%
-  production_mln$
+  production_mln
+
+production_world <- df %>%
+  filter(Date >= 2010) %>%
+  group_by(Date) %>%
+  summarise(total_value = sum(Value, na.rm = FALSE)) %>%
+  arrange(Date)
+
+print(production_world)
+
+fwrite(production_world, file = "production_world.csv", sep = "\t", quote = FALSE)
+
+production_world$Unit <- "mln tonnes"
+production_world$total_value <- production_world$total_value / 1000000
+
+production_mln <- read_tsv("C:\\Users\\User\\Documents\\GitHub\\Ilya Chakov\\Aluminium-Market\\Презентация\\Data\\Production\\production_mln.csv")
+
+print(production_mln)
+
+countries <- c("China", "Russia", "Canada", "India", "United Arab Emirates", "Australia", "United States", "Norway", "Bahrain", "Brazil")
+
+top_10_sum <- df %>%
+  filter(Date >= 2010, country %in% countries) %>%
+  group_by(Date) %>%
+  summarise(top_10_value = sum(Value, na.rm = FALSE)) %>%
+  arrange(Date)
+  
+top_10_sum$top_10_value <- top_10_sum$top_10_value / 1000000
+top_10_sum$Unit <- "mln tonnes"
+
+
+print(top_10_sum)
+
+production_mln$Others <- production_world$total_value - top_10_sum$top_10_value
+
+fwrite(production_mln, file = "production_mln.csv", sep = "\t", quote = FALSE)
